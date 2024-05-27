03b092fbbdbaee72ecce96e0a0f5cb1ed446915e
Sergiusz Ignatowski <s.ignatenko.2999@gmail.com>
2024-05-27 20:30:26 +0200

    Implemented luhn algorithm.

--- card_creation.py
+++ card_creation.py
@@ -6,9 +6,35 @@ class CardCreation:
     card_issuer_id = "400000"
 
     def __init__(self):
-        self.card_number = self.card_issuer_id + str(random.randint(1000000000, 9999999999))
+        self.card_number = self._generate_last_card_number()
         self.card_pin = str(random.randint(1000, 9999))
 
-    def print_card_details(self):
+    def print_card_details(self) -> None:
         print(f"Your card number:\n{self.card_number}")
         print(f"Your card PIN:\n{self.card_pin}")
+
+    def _generate_base_card_number(self) -> str:
+        return self.card_issuer_id + str(random.randint(100000000, 999999999))
+
+    def _generate_last_card_number(self) -> str:
+        card_num = self._generate_base_card_number()
+        processed_digits_sum = self.luhn_algorithm(
+            list(map(int, card_num))
+        )
+        last_digit = 10 - processed_digits_sum % 10 if processed_digits_sum != 0 else 0
+        card_num = card_num + str(last_digit)
+        return card_num
+
+    @staticmethod
+    def luhn_algorithm(digits: list) -> int:
+        total_sum = 0
+        num_digits = len(digits)
+        parity = num_digits % 2
+
+        for i, digit in enumerate(digits):
+            if i % 2 == parity:
+                digit *= 2
+                if digit > 9:
+                    digit -= 9
+            total_sum += digit
+        return total_sum
